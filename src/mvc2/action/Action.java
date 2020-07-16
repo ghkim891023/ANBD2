1bdcf93ac89df0677ecbc6ede97d8ec6d8b190c3
ghkim891023 <ghkim891023@gmail.com>
2020-07-16 12:44:05 +0900

    write.do만 매핑 해놓음

--- src/mvc2/action/Action.java
+++ src/mvc2/action/Action.java
@@ -1,5 +1,11 @@
 package mvc2.action;
 
-public interface Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mvc2.vo.ActionForward;
 
+public interface Action {
+	
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+		throws Exception;
 }
